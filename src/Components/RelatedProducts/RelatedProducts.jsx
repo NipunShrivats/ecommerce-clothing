e9c041e572fcc16a534cc6f7f9ea20b030581287
NipunShrivats <nipunrawat16@gmail.com>
2024-09-03 17:45:59 +0530

    changes on 3sep, commit no.4

--- src/Components/RelatedProducts/RelatedProducts.jsx
+++ src/Components/RelatedProducts/RelatedProducts.jsx
@@ -1,28 +1,32 @@
-import Rect from 'react'
+import Rect from "react";
 import "./RelatedProducts.css";
 import Item from "../../Components/Item/Item";
-import data_products from "../../assets/Frontend_Assets/data"
+import data_products from "../../assets/Frontend_Assets/data";
 
 export default function RelatedProducts() {
-    return (
-        <>
-            <div className='relatedProducts container'>
-                <div className='title-box'>
-                    <h1>Relared Products</h1>
-                    <div className='title-line'></div>
-                </div>
-                <div className='relatedProducts-item'>
-                    {data_products.map((item, i) => {
-                        return <Item
-                            key={i}
-                            id={item.id}
-                            name={item.name}
-                            image={item.image}
-                            new_price={item.new_price}
-                            old_price={item.old_price} />
-                    })}
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="relatedProducts container">
+        <div className="title-box">
+          <h1>Relared Products</h1>
+          <div className="title-line"></div>
+        </div>
+        <div className="relatedProducts-item">
+          {data_products.map((item, i) => {
+            return (
+              <Item
+                className="thisItem"
+                key={i}
+                id={item.id}
+                name={item.name}
+                image={item.image}
+                new_price={item.new_price}
+                old_price={item.old_price}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
