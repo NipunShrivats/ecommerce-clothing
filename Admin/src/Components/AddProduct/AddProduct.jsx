e78a01bc010e3453b6bb6ced6b58ae874ac6aa1e
NipunShrivats <nipunrawat16@gmail.com>
2024-09-06 18:17:26 +0530

    changes on 6sep, commit no.3

--- Admin/src/Components/AddProduct/AddProduct.jsx
+++ Admin/src/Components/AddProduct/AddProduct.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./AddProduct.css";
+import { FaUpload } from "react-icons/fa";
 
 export default function Addproducts() {
   return (
@@ -8,6 +9,38 @@ export default function Addproducts() {
         <div className="addproduct-itemfield">
           <p>Product title</p>
           <input type="text" name="name" placeholder="Enter product title.." />
+        </div>
+
+        <div className="addproduct-price">
+          <div className="addproduct-itemfield">
+            <p>Old Price</p>
+            <input type="text" name="old_price" placeholder="Type here" />
+          </div>
+          <div className="addproduct-itemfield">
+            <p>New Price</p>
+            <input type="text" name="new_price" placeholder="Type here" />
+          </div>
+          <div className="addproduct-itemfield">
+            <p>Product Category</p>
+            <select name="category" className="add-product-selector">
+              <option value="none">Select category</option>
+              <option value="men">Men</option>
+              <option value="women">Women</option>
+              <option value="Kids">Kids</option>
+            </select>
+          </div>
+          <div className="addproduct-itemfield">
+            <label htmlFor="file-input">
+              <div className="upload-container">
+                <span>
+                  <FaUpload />
+                </span>
+                <p>Upload Image</p>
+              </div>
+            </label>
+            <input type="file" name="image" id="file-input" hidden />
+            <button className="addproduct-btn">Add Product</button>
+          </div>
         </div>
       </div>
     </>
