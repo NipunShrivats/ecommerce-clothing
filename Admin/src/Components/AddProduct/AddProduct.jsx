7046050c4aff10c1bf652c13536db505e1823552
NipunShrivats <nipunrawat16@gmail.com>
2024-09-07 15:36:37 +0530

    changes on 7sep, commit no.1

--- Admin/src/Components/AddProduct/AddProduct.jsx
+++ Admin/src/Components/AddProduct/AddProduct.jsx
@@ -6,30 +6,53 @@ export default function Addproducts() {
   return (
     <>
       <div className="add-product">
+        <h2>
+          Add Product <hr />
+        </h2>
         <div className="addproduct-itemfield">
           <p>Product title</p>
-          <input type="text" name="name" placeholder="Enter product title.." />
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter product title.."
+            className="product-title-input"
+          />
         </div>
 
         <div className="addproduct-price">
           <div className="addproduct-itemfield">
             <p>Old Price</p>
-            <input type="text" name="old_price" placeholder="Type here" />
+            <input
+              type="text"
+              name="old_price"
+              placeholder="Type here"
+              className="old-price-input"
+            />
           </div>
           <div className="addproduct-itemfield">
             <p>New Price</p>
-            <input type="text" name="new_price" placeholder="Type here" />
+            <input
+              type="text"
+              name="new_price"
+              placeholder="Type here"
+              className="new-price-input"
+            />
           </div>
+        </div>
+
+        <div className="category-image">
           <div className="addproduct-itemfield">
             <p>Product Category</p>
             <select name="category" className="add-product-selector">
-              <option value="none">Select category</option>
+              <option value="none" className="option">
+                Select category
+              </option>
               <option value="men">Men</option>
               <option value="women">Women</option>
               <option value="Kids">Kids</option>
             </select>
           </div>
-          <div className="addproduct-itemfield">
+          <div className="addproduct-itemfield upload-container-keupar">
             <label htmlFor="file-input">
               <div className="upload-container">
                 <span>
@@ -39,9 +62,9 @@ export default function Addproducts() {
               </div>
             </label>
             <input type="file" name="image" id="file-input" hidden />
-            <button className="addproduct-btn">Add Product</button>
           </div>
         </div>
+        <button className="addproduct-btn">Add Product</button>
       </div>
     </>
   );
